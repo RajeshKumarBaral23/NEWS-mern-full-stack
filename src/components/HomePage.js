0849b1df2a3d5e1ad5c1bca9fe3e7156bb0f35e7
RajeshKumarBaral <147187698+RajeshKumarBaral23@users.noreply.github.com>
2025-01-16 00:52:29 +0530

    Update and rename Homepage.js to HomePage.js

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,9 +1,10 @@
+// filepath: /C:/Users/lemrk/Music/NEWS-mern-full-stack/src/components/HomePage.js
 import React from 'react';
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Welcome to CBN News</h1>
+      <h1>Welcome to M News</h1>
       <p>Browse through the latest news articles.</p>
     </div>
   );

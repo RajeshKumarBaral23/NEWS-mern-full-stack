33bf05df5348b91da4d34ace9ef9b79fc23e0f52
RajeshKumarBaral <147187698+RajeshKumarBaral23@users.noreply.github.com>
2025-01-16 00:54:00 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+// filepath: /C:/Users/lemrk/Music/NEWS-mern-full-stack/src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
